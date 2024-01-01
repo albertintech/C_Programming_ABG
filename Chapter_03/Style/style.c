c1ca0fc963658d20b955162a2f88e8dcb4da8bc1
Albert Ramos <albertramos.dev@gmail.com>
2024-01-01 08:46:12 -0700

    Add style.c and escape.c files

---
+++ Chapter_03/Style/style.c
@@ -0,0 +1,9 @@
+// Another Code Example, just with a different commenting style
+
+#include <stdio.h>
+
+int main()
+{
+	printf("I like these new comments!\n"); // A simple statement
+	return 0;
+}
---
+++ Chapter_04/escape.c
@@ -0,0 +1,13 @@
+/* These lines show you how to use the most popular Escscape Sequences */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Column A\tColumn B\tColumn C");
+	printf("\nMy Computer\'s Beep Sounds Like This: \a!\n");
+	printf("\"Letz\b\'s fix that typo and the show the backslash ");
+	printf("character \\\" she said\n");
+	
+	return 0;
+}
